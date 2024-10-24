22d644d1b6ae1a4946fb95f652e9c2b0f4045cea
FrancoA325 <fantunez04@gmail.com>
2024-10-24 15:50:24 -0400

    A5 done

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -30,24 +30,38 @@ function tires() {
 }
 
 function suspension() {
-    alert("Funky Ant is optimizing aerodynamics!");
+    alert("Funky Ant is optimizing suspension!");
 }
 
-function teamInfo() {
-    alert("Add some team info here!");
-    const teamName = prompt("Please enter your team name:");
-    const teamMembers = prompt("Please enter your team members (comma separated):");
-    const teamLocation = prompt("Please enter your team location:");
-    alert(`Team Name: ${teamName}\nTeam Members: ${teamMembers}\nTeam Location: ${teamLocation}`);
-    if (confirm("Is this information correct?")) {
+function price() {
+    alert("Funky Ant is calculating pricing!");
+}
+function teamInfoModal() {
+    document.getElementById("teamInfoModal").classList.add("show");
+}
+
+function submitTeamInfo() {
+    const teamName = document.getElementById("teamName").value;
+    const teamMembers = document.getElementById("teamMembers").value;
+    const teamLocation = document.getElementById("teamLocation").value;
+
+    const teamInfoMessage = `Team Name: ${teamName}\nTeam Members: ${teamMembers}\nTeam Location: ${teamLocation}`;
+
+    if (confirm(`${teamInfoMessage}\n\nIs this information correct?`)) {
         alert("Thank you for providing your team information!");
+        document.getElementById("teamInfoModal").classList.remove("show");
     }
 }
 
+function mailingModal() {
+    document.getElementById("mailingModal").classList.add("show");
+}
+
 function mailingList() {
-    alert("Welcome to the Funky Ant mailing list!");
-    const email = prompt("Please enter your email address:");
-    if (email) {
-        alert(`Thank you for joining the Funky Ant mailing list, ${email}!`);
+    const email = document.getElementById("email").value;
+
+    if (confirm(`Is ${email} the correct email address?`)) {
+        alert("Thank you for signing up for our mailing list!");
+        document.getElementById("mailingModal").classList.remove("show");
     }
 }
