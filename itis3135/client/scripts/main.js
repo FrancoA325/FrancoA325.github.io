7a51826f2754a99d495aa2c548d7938d011286e6
Franco Antunez <fantunez04@gmail.com>
2024-12-04 21:14:25 -0500

    pushing

--- itis3135/client/scripts/main.js
+++ itis3135/client/scripts/main.js
@@ -11,3 +11,17 @@ document.addEventListener("DOMContentLoaded", function() {
     loadHTML('header', 'components/header.html');
     loadHTML('footer', 'components/footer.html');
 });
+
+function validateHTML() {
+    const currentURL = encodeURIComponent(window.location.href);
+    window.open(`https://validator.w3.org/nu/?doc=${currentURL}`, "_blank");
+}
+
+function validateCSS() {
+    const currentURL = encodeURIComponent(window.location.href);
+    window.open(`https://jigsaw.w3.org/css-validator/validator?uri=${currentURL}&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en`, "_blank");
+}
+
+function validateAIM() {
+    window.open("https://webaim.org/search/?q=afsd", "_blank");
+}
